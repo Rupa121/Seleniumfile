eff3f8a2cf58e5dadd698a44b8560510bb70afe2
Rupa Jha <Rupa Jha@LAPTOP-S7KK6RGF>
2020-11-24 14:31:29 -0500

    assignment

--- seleniumlearning/src/test/java/com/testcases/Firefox.java
+++ seleniumlearning/src/test/java/com/testcases/Firefox.java
@@ -1,9 +1,25 @@
 package com.testcases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Firefox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.id("txtUsername")).sendKeys(" Admin ");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		driver.close();
 
 	}
 
